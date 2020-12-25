e726161b29e7fdf1fbe68d84f8d8af9949e7107c
seifallah <seifallah.ba@gmail.com>
2020-12-25 13:13:09 +0100

    created login component

---
+++ src/components/login/LoginForm.js
@@ -0,0 +1,57 @@
+import React, {useState} from "react";
+import { useHistory } from "react-router-dom";
+
+const LoginForm = () => {
+    const history = useHistory();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+  
+    const validateForm =() => {
+      return email.length > 0 && password.length > 0;
+    }
+  
+    const handleSubmit =(e) => {
+      e.preventDefault();
+      if(email=== process.env.REACT_APP_USER_EMAIL && password === process.env.REACT_APP_USER_PASSWORD) 
+        {
+            history.push('/todos')
+        }
+       else {
+        setError("error...");
+      }
+    }
+  
+    return (
+      <div className="loginForm--container">
+          <h1>{process.env.REACT_APP_USER_EMAIL}</h1>
+          <h1>{process.env.REACT_APP_USER_PASSWORD}</h1>
+        <form onSubmit={handleSubmit}>
+            <div class="form-group">
+                <label>Email</label>
+                <input
+                autoFocus
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                />
+          </div>
+          <div class="form-group">
+            <label>Password</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          {error&&<div className="loginForm-error">{error}</div>}
+          <button type="submit" disabled={!validateForm()}>
+            Login
+          </button>
+        </form>
+      </div>
+    );
+}
+
+export default LoginForm
---
+++ src/pages/Login.js
@@ -0,0 +1,9 @@
+import LoginForm from "../components/login/LoginForm";
+
+const LoginPage = () => {
+    return (<div className="loginPage--container"> 
+        <LoginForm />
+    </div>);
+}
+ 
+export default LoginPage;
